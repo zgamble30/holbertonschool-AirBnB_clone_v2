46a8521ccf96546a8be15975296309243f045100
Jaredglass3 <Jaredglass352@gmail.com>
2023-03-03 16:04:39 -0600

    Updated console

--- console.py
+++ console.py
@@ -114,17 +114,20 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        args = args.split()
+        if not args[0]:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        # creating a dict from args
+        new_dict = self._create_dict_instance(args[1:])
+        # sending args on form of kwargs
+        new_instance = HBNBCommand.classes[args[0]](**new_dict)
         print(new_instance.id)
-        storage.save()
-
+        new_instance.save()
+        
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
